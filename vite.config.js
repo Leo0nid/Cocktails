6e9832ab5d0e3c274c2283d8baec6bb2df2230b2
Leo0nid <121210862+Leo0nid@users.noreply.github.com>
2023-07-10 21:11:54 +0600

    add  -vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +10,11 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['element-plus']
     }
   }
-})
+});
